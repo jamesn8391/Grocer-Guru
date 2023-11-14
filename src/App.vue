02e71d3a50015e8f0dca38c3a64eab06f860f1d9
Jaden Reyes <jadenreyes417@gmail.com>
2023-11-14 16:44:07 -0600

    more navbar updates

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,15 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <header class="bg-dark">
-      <div class="container d-flex align-items-center justify-content-between">
-        <img alt="Guru logo" class="logo ms-auto" src="@/assets/OIG-removebg-preview.png" width="125" height="125" />
-        <nav class="d-flex">
-          <RouterLink class="nav-link ml-auto" to="/">Home</RouterLink>
-          <RouterLink class="nav-link" to="/about">About</RouterLink>
-        </nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container-fluid">
+        <img alt="Guru logo" class="logo" src="@/assets/OIG-removebg-preview.png" width="150" height="150" /> 
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/about" class="nav-link">About</router-link>
+        </div>
       </div>
-    </header>
+    </nav>
   </div>
 </template>
 
@@ -25,37 +25,33 @@ header {
   left: 0;
   width: 100%;
 }
-body {
-  background-color: #ffffff;
-  margin: 0;
-}
 
 .container {
   padding: 1rem;
 }
-
 .logo {
-  display: block;
+  display: inline-flex;
+  pointer-events: none;
 }
-
 nav {
   font-size: 20px;
   color: #4cda98;
+  height: 100px;
 }
-
+/* Remove the hover color for the logo by overriding Bootstrap styles */
+.navbar-dark .navbar-nav .nav-link:hover {
+  color: #4cda98; /* Set the color to the desired non-hover color */
+}
 .navbar-brand {
   color: #4cda98;
 }
-
 .nav-link {
   color: #4cda98;
   margin-left: 15px; /* Adjust the margin between links as needed */
 }
-
 .nav-link:last-child {
   margin-right: 0;
 }
-
 @media (min-width: 1024px) {
   header {
     display: flex;
