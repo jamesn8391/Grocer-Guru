c14aa8f83da54b7cfa26e78637487f54538c7a53
Jaden Reyes <jadenreyes417@gmail.com>
2023-11-16 17:10:50 -0600

    changes to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,25 @@
 </script>
 
 <template>
-    <div class="container mt-5 pt-5 text-center">
-    <p class="font-size-lg">Welcome to Grocer Guru!<br>Get started by adding items.</p>
+  <div class="container mt-5">
+    <div class="row bg-color">
+      <div class="col">
+        <img alt="Background" src="@/assets/background.jpg" style="width: 100%;">
+      </div>
+      <div class="col">
+        <div class="container mt-5 pt-5 text-center">
+          <h1>
+            Welcome to Grocer Guru!<br>Get started by adding items.
+          </h1>
+          <button @click="toggleGroceryList" class="btn btn-secondary mt-3">
+            <i class="fas fa-plus"></i> Add Grocery List
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div v-if="showGroceryList" class="container mt-5 pt-5 text-center">
     <div v-for="(searchBar, index) in searchBars" :key="index" class="form-outline">
       <input
         type="search"
@@ -14,10 +31,6 @@
       />
     </div>
 
-    <button @click="addSearchBar" class="btn btn-primary mt-3">
-      <i class="fas fa-plus"></i> Add Grocery Item
-    </button>
-    
     <button @click="saveAllItems" class="btn btn-success mt-3 ml-3">
       All Items Added
     </button>
@@ -29,21 +42,23 @@
 export default {
   data() {
     return {
-      searchBars: [{ id: 0 }] 
-    };
-    return {
-      groceryItems: [{ name: '' }]
+      showGroceryList: false,
+      searchBars: [{ id: 0 }],
+      groceryItems: []
     };
   },
   methods: {
+    toggleGroceryList() {
+      this.showGroceryList = !this.showGroceryList;
+    },
     addSearchBar() {
       const newIndex = this.searchBars.length;
       this.searchBars.push({ id: newIndex });
-      this.groceryItems.push({ name: '' });
-    }
-  },
-  saveAllItems() {
+      this.groceryItems.push();
+    },
+    saveAllItems() {
       console.log('All items:', this.groceryItems);
+    }
   }
 };
 </script>
@@ -51,10 +66,13 @@ export default {
 <style scoped>
 .font-size-lg {
   font-size: 2rem; 
+  font-family: sans-serif;
+}
+.bg-color {
+  background-color: #cedcdd;
 }
 
 .btn {
   margin-right: 10px;
 }
-
 </style>
