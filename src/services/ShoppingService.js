2744df8efca998df1656fa6a4739dcb01d8aac34
James Nolan <jamesn8391@gmail.com>
2023-11-16 16:46:20 -0600

    changed to export and added env

--- src/services/ShoppingService.js
+++ src/services/ShoppingService.js
@@ -1,24 +1,25 @@
+require('dotenv').config();
 const { getJson } = require("serpapi");
 
-getJson({
-  engine: "google",
-  api_key: '9c8df8889532b8222db45b170f14edd19d87869d21550103b838a15e64d2897a', 
-  tbm: "shop",
-  tbs: "mr:1,merchagg:g784994%7Cm10046|m122214550|g8299768%7Cm8175035|g126652263%7Cm117989436",
-  q: "potato chips",  
-  num: 100,
-  location: "College Station, Texas",
-}, (data) => {
-    const shoppingResults = data["shopping_results"]; // Correct variable name
+export function fetchShoppingResults(query, location) {
+  return new Promise((resolve, reject) => {
+    getJson({
+      engine: "google",
+      api_key: serpApiKey, 
+      tbm: "shop",
+      tbs: "mr:1,merchagg:g784994%7Cm10046|m122214550|g8299768%7Cm8175035|g126652263%7Cm117989436",
+      q: query,  
+      num: 100,
+      location: location,
+    }, (data) => {
+      const shoppingResults = data["shopping_results"];
+  
+      if (shoppingResults && Array.isArray(shoppingResults)) {
+        resolve(shoppingResults);
+      } else {
+        reject("No valid shopping results found in the JSON response.");
+      }
+    });
+  });
+}
 
-    if (shoppingResults && Array.isArray(shoppingResults)) {
-        shoppingResults.forEach((shoppingResult) => {
-            console.log(`Title: ${shoppingResult.title}`);
-            console.log(`Price: ${shoppingResult.price}`);
-            console.log(`Store: ${shoppingResult.source}`);
-            console.log(`Thumbnail: ${shoppingResult.thumbnail}`);
-        });
-    } else {
-        console.error("No valid shopping results found in the JSON response.");
-    }
-});
