8a59988b0320d6289fedd62c96710fe2286dc692
Jaden Reyes <jadenreyes417@gmail.com>
2023-12-01 20:00:43 -0600

    final final

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -16,9 +16,6 @@ import { RouterLink } from 'vue-router'
                         <li class="nav-item">
                             <router-link to="/" class="nav-link">Home</router-link>
                         </li>
-                        <li class="nav-item">
-                            <router-link to="/about" class="nav-link">About</router-link>
-                        </li>
                     </ul>
                 </div>
             </div>
